e7097955d85dbdb0013db4a0f920081107664e57
jose <josevegadev@gmail.com>
2019-04-01 14:05:30 -0600

    Added device-props.mk and device.mk

---
+++ device.mk
@@ -0,0 +1,73 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#Iwhat is this?
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p_mr1.mk)
+
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/xiaomi/perseus/perseus-vendor.mk)
+
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+#Check overlays 
+# Overlays
+#DEVICE_PACKAGE_OVERLAYS += \
+#   $(LOCAL_PATH)/overlay \
+#  $(LOCAL_PATH)/overlay-lineage
+
+
+# Properties
+-include $(LOCAL_PATH)/device-props.mk
+
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Inherit from sdm845-common
+$(call inherit-product, device/xiaomi/sdm845-common/sdm845.mk)
+
+#TODO get fstab
+# Device fstab
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
+#TODO: get this
+# Device init scripts
+PRODUCT_PACKAGES += \
+    init.target.rc
+#TODO whats this
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/etc/manifest.xml
+# add idc
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/idc/uinput-goodix.idc:system/usr/idc/uinput-goodix.idc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
+
+# NFC
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    com.nxp.nfc.nq \
+    nqnfcee_access.xml \
+    nqnfcse_access.xml \
+    NQNfcNci \
+    SecureElement \
+    Tag
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
--- lineage_perseus.mk
+++ lineage_perseus.mk
@@ -26,4 +26,5 @@ BUILD_FINGERPRINT := "Xiaomi/perseus/perseus:9/PKQ1.180729.001/9.3.28:user/relea
 #   PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys" \  #  PRODUCT_NAME="dipper" \
 # TARGET_DEVICE="dipper"
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+#TODO What is this 
+#PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
