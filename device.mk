d3ecb8976e93ae2682797d9715e5529ba6adfb0b
jose <josevegadev@gmail.com>
2019-04-01 22:41:28 -0600

    disabled treble props

--- device.mk
+++ device.mk
@@ -72,6 +72,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
+# Treble
+BOARD_VNDK_RUNTIME_DISABLE := false
+
 # PocketMode
 PRODUCT_PACKAGES += \
     XiaomiPocketMode
