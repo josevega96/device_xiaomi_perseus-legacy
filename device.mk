2ede7cffc43181038ed4a77a6ccdd99a3a416bee
Bruno Martins <bgcngm@gmail.com>
2019-09-24 17:05:28 +0100

    Perseus: Bring NFC NXP up to speed
    The soong namespaces must now be set so that the proper
    implementations are selected.
    
    Change-Id: Ic205daf36a1153d94e7d03a7693fb2583703e2b5
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

--- device.mk
+++ device.mk
@@ -81,12 +81,13 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     com.gsma.services.nfc \
     com.nxp.nfc.nq \
-    nfc_nci.nqx.default \
+    libnqnfc_nci_jni \
+    nfc_nci.nqx.default.hw \
+    NQNfcNci \
     nqnfcee_access.xml \
     nqnfcse_access.xml \
-    NQNfcNci \
     Tag \
-    vendor.nxp.hardware.nfc@1.1-service
+    vendor.nxp.hardware.nfc@1.2-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
