36fec98b5bfc7122bfdd1137968820dd8d40f376
josevega <jdelavega36@gmail.com>
2020-01-22 10:15:53 -0600

    Perseus: Inherit mainline configs
    sudo This aligns the product closer to the blueline and crosshatch builds.
    Specifically, the /system partition will converge to be identical
    once a few AOSP apps have been moved to /product.

--- potato_perseus.mk
+++ potato_perseus.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/mainline.mk)
+
 $(call inherit-product, device/xiaomi/perseus/device.mk)
 
 # Inherit some common POSP stuff.
