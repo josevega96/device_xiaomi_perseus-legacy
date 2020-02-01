6519044ba217c4955c5c889d3310b687387a4edc
josevega96 <jdelavega36@gmail.com>
2020-02-01 14:43:23 -0600

    Perseus: patch com.qti.node.debug and vidhance.node.eis.so

--- extract-files.sh
+++ extract-files.sh
@@ -13,6 +13,8 @@ fi
 
 set -e
 
+ROM_ROOT=../../..
+
 # Required!
 export DEVICE=perseus
 export DEVICE_COMMON=sdm845-common
@@ -20,8 +22,16 @@ export VENDOR=xiaomi
 
 export DEVICE_BRINGUP_YEAR=2019
 
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+
+BLOB_ROOT="$ROM_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
 # Load com.vidhance.node.eis shim
 VIDHANCE_EIS="$BLOB_ROOT"/vendor/lib/camera/components/com.vidhance.node.eis.so
 patchelf --add-needed com.vidhance.node.eis.shim.so "$VIDHANCE_EIS"
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+#Add libdng_sdk to com.qti.node.debug
+patchelf --add-needed libdng_sdk.so "º$BLOB_ROOT"/vendor/lib/camera/components/com.qti.node.dbug
+
+
